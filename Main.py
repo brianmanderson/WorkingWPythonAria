f7089c4dcfb3bdf650d24254d0fa58d8c58dd0d1
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-10-13 14:11:17 -0700

    update everything

--- Main.py
+++ Main.py
@@ -1,8 +1,15 @@
-import pyesapi
+from PyESAPI import pyesapi
 import atexit
+import os
 
 app = pyesapi.CustomScriptExecutable.CreateApplication('python_demo')
 atexit.register(app.Dispose)
-
+fid = open(os.path.join('.', 'PatientID'))
+line = fid.readline()
+fid.close()
+MRN = line.strip('\n')
+app.ClosePatient()
+patient = app.OpenPatientById(MRN)
+plan = patient.CoursesLot(0).PlanSetupsLot(0)
 pat = app.OpenPatientById('')
 xxx = 1
